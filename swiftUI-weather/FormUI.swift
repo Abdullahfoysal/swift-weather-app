b3dd339a74dc19b8d1598d24d64d6a25bea47448
Abdullah Al Foysal <abdullahfoysal32@gmail.com>
2023-07-19 20:19:21 +0600

    swift form ,input data

--- swiftUI-weather/FormUI.swift
+++ swiftUI-weather/FormUI.swift
@@ -12,19 +12,47 @@ struct FormUI: View {
     @State private var lastName = ""
     @State private var date = Date()
     @State private var shouldSendNewsletter: Bool = false
+    @State private var numberOfLikes: Int = 1
     var body: some View {
-       
-        Form {
-            Section(header: Text("Personal Information")){
-                TextField("First Name",text: $firstName)
-                TextField("Last Name",text: $lastName)
-                DatePicker("Birthdate",selection: $date, displayedComponents: .date)
+        
+        NavigationView {
+            Form {
+                Section(header: Text("Personal Information")){
+                    TextField("First Name",text: $firstName)
+                    TextField("Last Name",text: $lastName)
+                    DatePicker("Birthdate",selection: $date, displayedComponents: .date)
+                }
+                Section(header: Text("Actions")){
+                    Toggle("Send NewsLetter",isOn: $shouldSendNewsletter)
+                        .toggleStyle(SwitchToggleStyle(tint: .red))
+                    
+                    Stepper("Number of Likes",value: $numberOfLikes, in: 1...100)
+                    Text("This video has \(numberOfLikes) likes")
+                    Link("Terms of Service",destination: URL(string: "https://google.com")!)
+                }
+                
             }
-            Section(header: Text("Actions")){
-                Toggle("Send NewsLetter",isOn: $shouldSendNewsletter)
-            }
+             .navigationTitle("Account")
+             .toolbar {
+                 ToolbarItemGroup(placement: .navigationBarTrailing) {
+                     Button {
+                         hideKeyboard()
+                     }label: {
+                         Image(systemName: "keyboard.chevron.compact.down")
+                     }
+                     Button {
+                         print("SAVE data")
+                     }label: {
+                         Text("Save")
+                     }
+                 }
+             }.accentColor(.red)
+                
+               
             
         }
+       
+        
        
     }
 }
@@ -34,3 +62,12 @@ struct FormUI_Previews: PreviewProvider {
         FormUI()
     }
 }
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+
+
